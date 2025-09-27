979bb4e7afff79a8ee5c327926c4158e1eaa6f7a
Vishwajeet3007 <vishwa843113@gmail.com>
2025-09-27 13:12:53 +0530

    Update llm models

--- ChatBot_In_LangGraph/test.py
+++ ChatBot_In_LangGraph/test.py
@@ -1,9 +1,15 @@
-from google.generativeai import GenerativeModel
-
+import os
+from dotenv import load_dotenv
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyDH1MvDXs5Ooj5i6mqOsWFi8T1aE7GBg***")
+# Load .env file
+load_dotenv()
 
+# Get the key from environment variable
+api_key = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=api_key)
+
+# List available models
 models = genai.list_models()
 for model in models:
     print(model.name)
